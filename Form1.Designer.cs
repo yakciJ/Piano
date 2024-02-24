945a07ab901bc978d356fcdcd6e1722239c5e755
Jickay <sieunhan482@gmail.com>
2024-02-24 21:11:34 +0700

    add background

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            Key2Name = new Label();
+            label1 = new Label();
+            Key1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._24PianoKey;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1296, 600);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(28, 233);
+            label2.Name = "label2";
+            label2.Size = new Size(37, 38);
+            label2.TabIndex = 3;
+            label2.Text = "A";
+            // 
+            // Key2Name
+            // 
+            Key2Name.AutoSize = true;
+            Key2Name.BackColor = Color.White;
+            Key2Name.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Key2Name.Location = new Point(136, 233);
+            Key2Name.Name = "Key2Name";
+            Key2Name.Size = new Size(38, 38);
+            Key2Name.TabIndex = 4;
+            Key2Name.Text = "B";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Stencil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(244, 233);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 38);
+            label1.TabIndex = 5;
+            label1.Text = "C";
+            // 
+            // Key1
+            // 
+            Key1.Location = new Point(0, 0);
+            Key1.Margin = new Padding(0);
+            Key1.Name = "Key1";
+            Key1.Size = new Size(95, 287);
+            Key1.TabIndex = 0;
+            Key1.Text = "Key1";
+            Key1.UseVisualStyleBackColor = true;
+            Key1.Visible = false;
+            Key1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1280, 601);
+            Controls.Add(Key1);
+            Controls.Add(label1);
+            Controls.Add(Key2Name);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label Key2Name;
+        private Label label1;
+        private Button Key1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,23 @@
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+
 namespace Piano
 {
     public partial class Form1 : Form
     {
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) { }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.URL = @"C:\Users\Administrator\OneDrive\Documents\Coding\CsProject\Piano\bin\Debug\Sound\key01.mp3";
+            player.controls.play();
         }
     }
 }
